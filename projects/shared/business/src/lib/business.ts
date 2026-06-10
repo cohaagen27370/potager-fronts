ba6771ebadd8f8590673c5197f17c47171724f81
Laurent BLANQUET <laurentblanquet@outlook.fr>
2026-06-10 19:53:30 +0200

    initial commit

---
+++ projects/shared/business/src/lib/business.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-business',
+  imports: [],
+  template: `
+    <p>
+      business works!
+    </p>
+  `,
+  styles: ``,
+})
+export class Business {
+
+}
---
+++ projects/shared/business/src/public-api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of business
+ */
+
+export * from './lib/business';
