7fa23d682438e2435fdcb7d8115f767695103d8b
Laurent BLANQUET <laurentblanquet@outlook.fr>
2026-06-11 18:56:44 +0200

    fixing code

--- projects/apps/inprogress/src/app/app.ts
+++ projects/apps/inprogress/src/app/app.ts
@@ -1,16 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { Layout } from '@ui';
-import { environment as Environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   imports: [Layout],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('inprogress');
-
-  environment = Environment;
-
 }
